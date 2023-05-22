b1b49feefe22a9dec9dbac54649e1f5fc991bbd8
CloveRs212 <cloverindustrys@gmail.com>
2023-05-21 21:06:28 -0400

    Shop Page final

--- Courses/Complete React Dev/retail_project/src/routes/categories-preview/categories-preview.component.jsx
+++ Courses/Complete React Dev/retail_project/src/routes/categories-preview/categories-preview.component.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, Fragment } from 'react';
 
 import CategoryPreview from '../../components/category-preview/category-preview.component';
 import { CategoriesContext } from '../../contexts/categories.context';
@@ -7,12 +7,12 @@ const CategoriesPreview = () => {
   const { categoriesMap } = useContext(CategoriesContext);
 
   return (
-    <>
+    <Fragment>
       {Object.keys(categoriesMap).map((title) => {
         const products = categoriesMap[title];
         return <CategoryPreview key={title } title={title} products={products}/>
       })}
-    </>
+    </Fragment>
   );
 };
 
