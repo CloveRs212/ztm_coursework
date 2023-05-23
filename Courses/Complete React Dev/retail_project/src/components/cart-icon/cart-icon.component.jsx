1f09bb14112718e06f3f214a42a5e32091f5c9f5
CloveRs212 <cloverindustrys@gmail.com>
2023-05-23 05:53:46 -0400

    Adding Styled Components

--- Courses/Complete React Dev/retail_project/src/components/cart-icon/cart-icon.component.jsx
+++ Courses/Complete React Dev/retail_project/src/components/cart-icon/cart-icon.component.jsx
@@ -5,7 +5,7 @@ import { ReactComponent as ShoppingIcon } from '../../assests/shopping-bag.svg';
 import { CartContext } from '../../contexts/cart.context';
 
 
-import './cart-icon.styles.scss';
+import { CartIconContainer, ItemCount } from './cart-icon.styles';
 
 
 const CartIcon = () => {
@@ -13,10 +13,10 @@ const CartIcon = () => {
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
   return (
-    <div className='cart-icon-container' onClick={toggleIsCartOpen}>
+    <CartIconContainer onClick={toggleIsCartOpen}>
       <ShoppingIcon className='shopping-icon' />
-      <span className='icon-content'>{cartCount}</span>
-    </div>
+      <ItemCount>{cartCount}</ItemCount>
+    </CartIconContainer>
   )
 }
 
